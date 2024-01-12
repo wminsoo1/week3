e279d29480e78d332eeaa55b11efe1041b548ebf
jjjh02 <jinnyshur@korea.ac.kr>
2024-01-12 15:32:13 +0900

    signup 기능 구현

--- src/main/java/madcamp/week3/controller/UserController.java
+++ src/main/java/madcamp/week3/controller/UserController.java
@@ -2,15 +2,22 @@ package madcamp.week3.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import madcamp.week3.model.User;
+//import madcamp.week3.service.UserService;
+import madcamp.week3.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
 public class UserController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/user/login")
     public String getLogin(Model model){
@@ -19,8 +26,24 @@ public class UserController {
     }
 
     @PostMapping("/user/login/details")
-    public String postDetailsLogin(@ModelAttribute User user){
-        //log.info("user: {}", user.toString());
+    public String postDetailsLogin(@ModelAttribute User user, HttpSession session){
+        log.info("user: {}", user.toString());
+        session.setAttribute("user",user);
         return "postLoginDetails";
     }
+
+    @PostMapping("/user/saveDetails")
+    public String saveUserDetails(@ModelAttribute User userDetails, HttpSession session) {
+        log.info("userDetails: {}", userDetails.toString());
+        User user = (User) session.getAttribute("user");
+        user.setEducation(userDetails.getEducation());
+        user.setAward1(userDetails.getAward1());
+        user.setAward2(userDetails.getAward2());
+        user.setOneLineProfile(userDetails.getOneLineProfile());
+        user.setGithubUrl(userDetails.getGithubUrl());
+        log.info("user: {}", user.toString());
+        userService.saveUserDetails(user);
+        return "redirect:/user/login";
+    }
+
 }
--- src/main/java/madcamp/week3/service/UserService.java
+++ src/main/java/madcamp/week3/service/UserService.java
@@ -1,8 +1,22 @@
 package madcamp.week3.service;
 
+import madcamp.week3.model.User;
+import madcamp.week3.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUserDetails(User user) {
+        // 사용자 정보를 데이터베이스에 저장
+        userRepository.save(user);
+    }
 
 }
