d3b0c3f65983eaff8e077a7a7ef90c62b2506d95
wminsoo1 <wminsoo1@naver.com>
2024-01-12 10:26:55 +0900

    데이터베이스 업데이트

--- src/main/java/madcamp/week3/model/Post.java
+++ src/main/java/madcamp/week3/model/Post.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,17 +19,25 @@ public class Post {
 
     private String postDescription;
 
+    @OneToOne
+    @JoinColumn(name = "project_id") // 외래 키 컬럼명
+    private Project project;
+
     @ManyToOne
     @JoinColumn(name = "user_id") // 외래 키 컬럼명
     private User user;
 
+    @OneToMany(mappedBy = "post") // 양방향 관계 설정
+    private List<Comment> comment;
+
     public Post() {
     }
 
-    public Post(Long postId, String postTitle, String postDescription, User user) {
+    public Post(Long postId, String postTitle, String postDescription, Project project, User user) {
         PostId = postId;
         this.postTitle = postTitle;
         this.postDescription = postDescription;
+        this.project = project;
         this.user = user;
     }
 
@@ -38,6 +47,7 @@ public class Post {
                 "PostId=" + PostId +
                 ", postTitle='" + postTitle + '\'' +
                 ", postDescription='" + postDescription + '\'' +
+                ", project=" + project +
                 ", user=" + user +
                 '}';
     }
