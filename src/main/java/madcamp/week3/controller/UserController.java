745d47194beb9b12cc59e655616886d2688dc0f0
jjjh02 <jinnyshur@korea.ac.kr>
2024-01-12 21:39:54 +0900

    userprofile 추가

--- src/main/java/madcamp/week3/controller/UserController.java
+++ src/main/java/madcamp/week3/controller/UserController.java
@@ -1,6 +1,7 @@
 package madcamp.week3.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import madcamp.week3.model.Project;
 import madcamp.week3.model.User;
 //import madcamp.week3.service.UserService;
 import madcamp.week3.service.UserService;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -52,11 +54,40 @@ public class UserController {
         return "getLogin";
     }
     @PostMapping("/user/login")
-    public String postLogin(@ModelAttribute User user){
+    public String postLogin(@ModelAttribute User user, HttpSession session){
         log.info("userLogin: {}", user.toString());
         // 여기까지는 출력
-        if(userService.loginUser(user)){return "redirect:/post";}
+        User loggedInUser = userService.loginUser(user);
+        if(loggedInUser != null){
+            session.setAttribute("loggedInUser", loggedInUser);
+            log.info("userLogin: {}", loggedInUser.toString());
+            return "redirect:/user/profile";}///////projecList로 테스트 redirect:/post
         else {return "getLogin";}
     }
+
+    @GetMapping("/user/projects")
+    public String getUserProjects(HttpSession session, Model model) {
+        Long userId = (Long) session.getAttribute("user");
+        List<Project> projects = userService.getProjectsByUserId(userId);
+        model.addAttribute("projects", projects);
+        return "projectList"; // Thymeleaf 템플릿 파일명
+    }
+
+    @GetMapping("/user/profile")
+    public String getUserProfile(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("loggedInUser");
+        if (user != null) {
+            log.info("userprofile1: {}", user.toString());
+            User checkuser = userService.getUserByUserId(user.getId());
+            log.info("userprofile2: {}", checkuser.toString());
+
+            model.addAttribute("user", checkuser); // Add the user to the model
+            return "userProfile";
+        } else {
+            // Handle the case where there is no user in the session
+            // This could be redirecting to a login page or showing an error message
+            return "redirect:/getLogin";
+        }
+    } // userid, pwd받아오는데 나머지 정보 못받아옴
 
 }
--- src/main/java/madcamp/week3/service/UserService.java
+++ src/main/java/madcamp/week3/service/UserService.java
@@ -1,9 +1,13 @@
 package madcamp.week3.service;
 
+import madcamp.week3.model.Project;
 import madcamp.week3.model.User;
 import madcamp.week3.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -19,11 +23,22 @@ public class UserService {
         userRepository.save(user);
     }
 
-    public boolean loginUser(User user) {
+    public User loginUser(User user) {
         User checkUser = userRepository.findByUserNameAndPassword(user.getUserName(), user.getPassword());
 
-        if (checkUser != null) {return true;}
-        else{return false;}
+        if (checkUser != null) {return checkUser;}
+        else{return null;}
+    }
+
+    public List<Project> getProjectsByUserId(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        return user.getProjectList();
+    }
+
+    public User getUserByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with id: " + userId));
+        return user;
     }
 
 }
