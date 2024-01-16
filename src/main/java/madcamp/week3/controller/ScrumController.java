b939dcfa3479879c9ccdcad5c6e8f295ce24310a
jjjh02 <jinnyshur@korea.ac.kr>
2024-01-16 13:16:37 +0900

    scrum 기능만 완료

--- src/main/java/madcamp/week3/controller/ScrumController.java
+++ src/main/java/madcamp/week3/controller/ScrumController.java
@@ -2,7 +2,9 @@ package madcamp.week3.controller;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import madcamp.week3.model.Project;
 import madcamp.week3.model.Scrum;
+import madcamp.week3.service.ProjectSerivce;
 import madcamp.week3.service.ScrumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -21,11 +24,36 @@ public class ScrumController {
 
     @Autowired
     private ScrumService scrumService;
+    @Autowired
+    private ProjectSerivce projectSerivce;
+
     @GetMapping("/scrum/details/{projectId}")
-    public String getScrumList(@PathVariable Long projectId, Model model, HttpSession session){
+    public String getScrumList(@PathVariable Long projectId, Model model){
+        // pathVariable로 받은 projectId로 scrumList 찾기
         List<Scrum> scrumList = scrumService.findScrumsByProjectId(projectId);
+        // scrumList를 model에 저장
         model.addAttribute("scrumList", scrumList);
+        // scrumDetail.html로
         return "scrumDetail";
     }
 
+    @GetMapping("/scrum/add")
+    public String getAddScrum(Model model, @RequestParam("projectId") Long projectId, HttpSession session){
+        // scrumDetail에서 requestParam으로 넘긴 projectId를 model에 저장
+        // scrum 새로 생성할것이므로 new Scrum()
+        model.addAttribute("scrum", new Scrum());
+        model.addAttribute("projectId", projectId);
+        return "addScrum";
+    }
+
+    @PostMapping("/scrum/add")
+    public String postAddScrum(Scrum scrum, @RequestParam("projectId") Long projectId){
+        log.info("checkprojectId3" + projectId.toString());
+        Project project = projectSerivce.findProjectById(projectId);
+        scrum.setProject(project);
+        scrumService.saveScrum(scrum);
+        return "redirect:/scrum/details/" + projectId;
+    }
+
 }
+
